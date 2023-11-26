2bf806cc79e4a5ca0eacab796e985db1fded4574
Loproxi <79161178+Loproxi@users.noreply.github.com>
2023-11-26 09:04:54 +0100

    Deserialize script

---
+++ PlantGame/Assets/scripts/Deserialize.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlantData
+{
+
+    public int id;
+    public string Name;
+    public float humidity;
+    public float airHumidity;
+    public float light;
+    public float temperature;
+
+}
+
+public class Deserialize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    PlantData Deserialize(string data)
+    {
+        jsonUtility
+    }
+}
